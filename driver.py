b9a2b9d97aab98f8f0fa3ebedce87866d4bc42b9
Roman Pena <rpena003@odu.edu>
2025-03-04 17:13:11 -0500

    switched from list of lists to multiple list for core temps

--- driver.py
+++ driver.py
@@ -1,31 +1,33 @@
 import sys
 
 from parse_temps import parse_raw_temps
-
+from piecewise_linear_interpolation import piecewise_linear_interpolation
 def main():
 
     input_temps = sys.argv[1]
 
-    """
-        with open(input_temps, "r") as temps_file:
-        # output raw structure
-        for temps_as_floats in parse_raw_temps(temps_file):
-            print(temps_as_floats)
-    """
-
     with open(input_temps, "r") as temps_file:
-        # split data
-
+        # ----------------------------------------------------------------------
+        # Split Data
+        # ----------------------------------------------------------------------
         times = []
-        core_data = [[] for _ in range(0, 4)]
-
-        for time, raw_core_data in parse_raw_temps(temps_file):
+        core_0_data = []
+        core_1_data = []
+        core_2_data = []
+        core_3_data = []
+        for time, core_data in parse_raw_temps(temps_file):
             times.append(time)
-            for core_idx, reading in enumerate(raw_core_data):
-                core_data[core_idx].append(reading)
+            core_0_data.append(core_data[0])
+            core_1_data.append(core_data[1])
+            core_2_data.append(core_data[2])
+            core_3_data.append(core_data[3])
 
-        for time, *temps in list(zip(times, *core_data))[4:]:
+        for time, *temps in list(
+            zip(times, core_0_data, core_1_data, core_2_data, core_3_data)
+        ):
             print(f"{time=} {temps=}")
+
+        piecewise_linear_interpolation(times, core_0_data)
 
 if __name__ == "__main__":
     main()
--- piecewise_linear_interpolation.py
+++ piecewise_linear_interpolation.py
@@ -1,11 +1,17 @@
+# core number coresponds to colum
 
-# start with y = mx + b
+# readings taken every 30 seconds, time corresponds to rows 
 
-# swap linear and constant terms y = b + mx
+def piecewise_linear_interpolation(times, core_data):
+    # x_k and x_k+1 is equivalent to the time a reading is taken
+    # y_k and y_k+1 are equivalent to core temps at different times
+    #for (x0, x1), (y0, y1) in list(zip(times[-1:], times[1:]), zip(core_data[-1:], core_data[1:])):
+        #slope = (y1 - y0) / (x1 - x0)
 
-# slope m = (y_1 - y_0) / (x_1 - x_0)
+       #b = y1 - slope * x1
+                
 
-# replace 0 with k, 1 with k + 1
-# y_k+1 - y_k
 
-# b = y_k - mx_k
+    #coefficients = (b, slope)
+
+    return times
