7481c755efa1222f9f0357410aea4436511069fd
Roman Pena <rpena003@odu.edu>
2025-03-04 12:23:54 -0500

    read in and split the data using functions from parse_temps_demo.py

--- driver.py
+++ driver.py
@@ -6,9 +6,26 @@ def main():
 
     input_temps = sys.argv[1]
 
-    with open(input_temps, "r") as temps_file:
+    """
+        with open(input_temps, "r") as temps_file:
+        # output raw structure
         for temps_as_floats in parse_raw_temps(temps_file):
             print(temps_as_floats)
+    """
+
+    with open(input_temps, "r") as temps_file:
+        # split data
+
+        times = []
+        core_data = [[] for _ in range(0, 4)]
+
+        for time, raw_core_data in parse_raw_temps(temps_file):
+            times.append(time)
+            for core_idx, reading in enumerate(raw_core_data):
+                core_data[core_idx].append(reading)
+
+        for time, *temps in list(zip(times, *core_data))[4:]:
+            print(f"{time=} {temps=}")
 
 if __name__ == "__main__":
     main()
